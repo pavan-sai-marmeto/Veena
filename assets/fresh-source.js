7e3664fa64023dc88788bcdf1ae8b1ef8ce17482
mm-pavan <pavan.sai@marmeto.com>
2024-03-07 09:14:44 +0530

    Added tab switching in fresh source page

--- assets/fresh-source.js
+++ assets/fresh-source.js
@@ -1,21 +1,12 @@
-const tabs = document.querySelectorAll(".fresh-source__collection-tabs li");
-tabs.forEach((tab) => {
-  tab.addEventListener("click", function () {
-    tabs.forEach((tab) => {
-      tab.classList.remove("active");
-    });
-    this.classList.add("active");
-  });
-});
-	document.querySelectorAll(".fresh-source__collection").forEach(element=>{
-		element.addEventListener("click",function(e){
-			document.querySelectorAll(".fresh-source__collection-content").forEach(container => {
-				if (element.dataset.id === container.dataset.index ){
-					container.classList.add("active-collection")
-				}else{
-					container.classList.remove("active-collection")
-				}
-				
-			})
-		})
-	})
+document.querySelectorAll(".fresh-source__collection-tab").forEach(element=>{
+    element.addEventListener("click",function(e){
+        e.preventDefault();
+    document.querySelectorAll(".fresh-source__collection-tab").forEach(each => each.classList.remove('active-tab'))
+        document.querySelectorAll(".fresh-source__collection-content").forEach(container => {
+            if (element.dataset.tabId === container.dataset.collectionId ){
+        element.classList.add('active-tab')
+        document.querySelectorAll(".fresh-source__collection-content").forEach(each=> each.classList.remove('active-collection'))
+        container.classList.add('active-collection')
+    }
+    })
+})})
